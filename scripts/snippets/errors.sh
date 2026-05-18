c17ea2f31c3d8f108c7d2f3223e8f447b47cd642
Sébastien Menard <sebastien.menard.github@gmail.com>
2026-05-18 18:51:14 -0400

    style(bash): format bash scripts using shfmt formatter

--- scripts/snippets/errors.sh
+++ scripts/snippets/errors.sh
@@ -8,27 +8,27 @@
 # It would find "my_function".
 # The return format: "<caller>:<file>:<line>"
 find_error_caller() {
-    local caller
-    local source="${1:-}"
-    local depth=${#FUNCNAME[@]}
+  local caller
+  local source="${1:-}"
+  local depth=${#FUNCNAME[@]}
 
-    for ((i = 1; i < depth; i++)); do
-        caller="${FUNCNAME[$i]}"
+  for ((i = 1; i < depth; i++)); do
+    caller="${FUNCNAME[$i]}"
 
-        # Skip the "throw_error" method
-        [[ "$caller" == "throw_error" ]] && continue
+    # Skip the "throw_error" method
+    [[ "$caller" == "throw_error" ]] && continue
 
-        # Skip custom "error_" methods, otherwise exit
-        if [[ ! "$caller" =~ ^error_ || "$caller" == "$source" ]]; then
-            local file="${BASH_SOURCE[$i]##*/}"
-            local line="${BASH_LINENO[$((i - 1))]:-?}"
-            printf "%s:%s:%s\n" "$caller" "$file" "$line"
-            return 0
-        fi
-    done
+    # Skip custom "error_" methods, otherwise exit
+    if [[ ! "$caller" =~ ^error_ || "$caller" == "$source" ]]; then
+      local file="${BASH_SOURCE[$i]##*/}"
+      local line="${BASH_LINENO[$((i - 1))]:-?}"
+      printf "%s:%s:%s\n" "$caller" "$file" "$line"
+      return 0
+    fi
+  done
 
-    printf "main:%s\n" "${BASH_SOURCE[$((${#BASH_SOURCE[@]} - 1))]##*/}"
-    return 0
+  printf "main:%s\n" "${BASH_SOURCE[$((${#BASH_SOURCE[@]} - 1))]##*/}"
+  return 0
 }
 
 # Print an error message with details.
@@ -36,74 +36,74 @@ find_error_caller() {
 # [SOURCE] error_missing_argument:utilities.sh:57
 # [HINT] Provide the name of the missing argument.
 error_message() {
-    local error="$1"
-    local hint="${2:-}"
-    local source="$(find_error_caller "${3:-}")"
+  local error="$1"
+  local hint="${2:-}"
+  local source="$(find_error_caller "${3:-}")"
 
-    {
-        printf "\n"
+  {
+    printf "\n"
 
-        # Print error
-        printf "[ERROR] %s\n" "$error"
+    # Print error
+    printf "[ERROR] %s\n" "$error"
 
-        # Print source
-        printf "[SOURCE] %s\n" "$source"
+    # Print source
+    printf "[SOURCE] %s\n" "$source"
 
-        # Print hint (optionnal)
-        [[ -n "$hint" ]] && printf "[HINT] %s\n" "$hint"
+    # Print hint (optionnal)
+    [[ -n "$hint" ]] && printf "[HINT] %s\n" "$hint"
 
-        printf "\n"
-    } >&2
+    printf "\n"
+  } >&2
 }
 
 # Throw an error with a custom message.
 throw_error() {
-    local error="$1"
-    local hint="${2:-}"
-    local source="${3:-}"
-    local code="${4:-2}"
+  local error="$1"
+  local hint="${2:-}"
+  local source="${3:-}"
+  local code="${4:-2}"
 
-    error_message "$error" "$hint" "$source"
+  error_message "$error" "$hint" "$source"
 
-    exit "$code"
+  exit "$code"
 }
 
 # Throw an error for a missing argument in a function call.
 error_missing_argument() {
-    local name="${1:-}"
+  local name="${1:-}"
 
-    [[ -z "$name" ]] && {
-        throw_error \
-            "Missing argument name." \
-            "Provide the name of the missing argument." \
-            "error_missing_argument"
-    }
+  [[ -z "$name" ]] && {
+    throw_error \
+      "Missing argument name." \
+      "Provide the name of the missing argument." \
+      "error_missing_argument"
+  }
 
-    throw_error "Missing argument: $name." "${2:-}" "${3:-}"
+  throw_error "Missing argument: $name." "${2:-}" "${3:-}"
 }
 
 # Throw an error for a missing path in a function call.
 error_missing_path() {
-    local path="${1:-}"
+  local path="${1:-}"
 
-    [[ -z "$path" ]] && error_missing_argument \
-        "path" \
-        "Provide the missing path."
+  [[ -z "$path" ]] && error_missing_argument \
+    "path" \
+    "Provide the missing path."
 
-    throw_error \
-        "Missing path: $path" \
-        "Make sure the file or directory exists."
+  throw_error \
+    "Missing path: $path" \
+    "Make sure the file or directory exists."
 }
 
 # Throw an error for a missing marker in the readme file.
 error_missing_marker() {
-    local marker="${1:-}"
+  local marker="${1:-}"
 
-    [[ -z "$marker" ]] && error_missing_argument \
-        "marker" \
-        "Provide the README marker that should be verified."
+  [[ -z "$marker" ]] && error_missing_argument \
+    "marker" \
+    "Provide the README marker that should be verified."
 
-    throw_error \
-        "Missing marker: $marker" \
-        "The README file must include the marker."
+  throw_error \
+    "Missing marker: $marker" \
+    "The README file must include the marker."
 }
--- scripts/snippets/index.sh
+++ scripts/snippets/index.sh
@@ -8,71 +8,71 @@ source "$SCRIPT_DIR/errors.sh"
 source "$SCRIPT_DIR/readme.sh"
 
 help() {
-    printf "\n"
-    printf "%s\n" \
-        "Usage: $0 --lang=<dir> --root=<dir>" \
-        "Example: $0 --lang=cpp --root=gtest" \
-        "Generates the \`gtest\` snippets for the \`cpp\` language." \
-        "Updates the README.md file under the path /snippets/cpp/gtest."
+  printf "\n"
+  printf "%s\n" \
+    "Usage: $0 --lang=<dir> --root=<dir>" \
+    "Example: $0 --lang=cpp --root=gtest" \
+    "Generates the \`gtest\` snippets for the \`cpp\` language." \
+    "Updates the README.md file under the path /snippets/cpp/gtest."
 }
 
 # Read CLI arguments and update the README file
 main() {
-    local dir_lang=""
-    local dir_root=""
+  local dir_lang=""
+  local dir_root=""
 
-    while [[ "$#" -gt 0 ]]; do
-        case "$1" in
-            --lang=*)
-                dir_lang="${1#*=}"
-                ;;
-            --lang)
-                if [[ "$#" -lt 2 ]]; then
-                    help >&2
-                    throw_error \
-                        "Missing value for --lang." \
-                        "Example: $0 --lang=cpp --root=gtest"
-                fi
-
-                dir_lang="${2:-}"
-                shift
-                ;;
-            --root=*)
-                dir_root="${1#*=}"
-                ;;
-            --root)
-                if [[ "$#" -lt 2 ]]; then
-                    help >&2
-                    throw_error \
-                        "Missing value for --root." \
-                        "Example: $0 --lang=cpp --root=gtest"
-                fi
-
-                dir_root="${2:-}"
-                shift
-                ;;
-            -h|--help)
-                help
-                exit 0
-                ;;
-            *)
-                throw_error \
-                    "Unknown parameter: $1" \
-                    "Run with --help to see usage."
-                ;;
-        esac
-
-        shift
-    done
-
-    if [[ -z "$dir_lang" || -z "$dir_root" ]]; then
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+    --lang=*)
+      dir_lang="${1#*=}"
+      ;;
+    --lang)
+      if [[ "$#" -lt 2 ]]; then
         help >&2
         throw_error \
-            "--lang and --root flags are required." \
-            "Example: $0 --lang=cpp --root=gtest"
-    fi
+          "Missing value for --lang." \
+          "Example: $0 --lang=cpp --root=gtest"
+      fi
 
-    update_readme "$dir_lang" "$dir_root"
+      dir_lang="${2:-}"
+      shift
+      ;;
+    --root=*)
+      dir_root="${1#*=}"
+      ;;
+    --root)
+      if [[ "$#" -lt 2 ]]; then
+        help >&2
+        throw_error \
+          "Missing value for --root." \
+          "Example: $0 --lang=cpp --root=gtest"
+      fi
+
+      dir_root="${2:-}"
+      shift
+      ;;
+    -h | --help)
+      help
+      exit 0
+      ;;
+    *)
+      throw_error \
+        "Unknown parameter: $1" \
+        "Run with --help to see usage."
+      ;;
+    esac
+
+    shift
+  done
+
+  if [[ -z "$dir_lang" || -z "$dir_root" ]]; then
+    help >&2
+    throw_error \
+      "--lang and --root flags are required." \
+      "Example: $0 --lang=cpp --root=gtest"
+  fi
+
+  update_readme "$dir_lang" "$dir_root"
 }
 
 main "$@"
